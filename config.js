6897a70736a24286bf2e040ab25aa680cbb228e9
yourFriendlyEngineer <96051672+yourFriendlyEngineer@users.noreply.github.com>
2026-01-05 21:16:19 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+const SITE_CONFIG = [
+    { 
+        folder: "silhouette",   // <--- КАВЫЧКИ ОБЯЗАТЕЛЬНЫ!
+        title: "Silhouette & Soul" 
+    },
+	{ 
+        folder: "manme", 
+        title: "Overthinker" 
+    },
+	{ 
+        folder: "edges", 
+        title: "Edges" 
+    },
+	{ 
+        folder: "excellence", 
+        title: "Excellence" 
+    },
+	{ 
+        folder: "greece", 
+        title: "Greece" 
+    },
+
+];
